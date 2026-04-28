8fd389eb35f83fea1412d99ffc5e136f86a39377
Vitrolo <viniciusrabelodominguesdasilva@gmail.com>
2026-04-27 21:28:33 -0300

    semaforo

--- lacos.py
+++ lacos.py
@@ -1,1 +1,10 @@
-print("Olá GitHub")
+sinal =input("Digite o sinal")
+
+if sinal == "veremelho":
+    print("PARE!")
+elif sinal == "amarelo":
+    print("ATENÇÃO")
+elif sinal == "verde":
+    print("SIGA EM FRENTE")
+else:
+    print("Que sinal é esse?")
